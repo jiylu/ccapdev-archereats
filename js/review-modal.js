6ed202aba501cf659941ef4d22825f243d8396df
jilyu <jeremy_leano@dlsu.edu.ph>
2026-02-08 03:17:06 +0800

    refine modal ui

--- js/review-modal.js
+++ js/review-modal.js
@@ -1,10 +1,29 @@
+let radioState = {};
+
+$('.feedback-buttons input[type="radio"]').each(function() {
+    if (this.checked) {
+        radioState[this.name] = this;
+    }
+});
+
 $(document).ready(function (){
     $(".write-review").click(function () {
-        $(".write-review-modal").fadeIn();
+        $(".write-review-modal").fadeIn(400);
     });
 
-    $(".close").click(function () {
-        $(".write-review-modal").fadeOut();
+    $(".button-cancel").click(function () {
+        $(".write-review-modal").fadeOut(200);
+    });
+
+    $('.feedback-buttons input[type="radio"]').on('click', function() {
+        let groupName = $(this).attr('name');
+
+        if (radioState[groupName] === this) {
+            $(this).prop('checked', false); 
+            radioState[groupName] = null;   // Clear the memory
+        } else {
+            radioState[groupName] = this;
+        }
     });
 })
 
