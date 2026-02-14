426006e57a9d910c50166a1b59100579a0b40e1a
jilyu <jeremy_leano@dlsu.edu.ph>
2026-02-14 16:09:53 +0800

    finish up restaurant card component, added landing page route

--- client/app/App.tsx
+++ client/app/App.tsx
@@ -1,18 +1,16 @@
 //import { useState } from 'react'
-import RestaurantCard from "./components/RestaurantCard"
+
+import { Route, Routes } from "react-router-dom"
+import Landing from "./pages/Landing"
 
 
 
 function App() {
 	return (
-		<>
-			<RestaurantCard
-				url="https://static.where-e.com/Philippines/Metro_Manila/Malate/The-Barn_d37b8917af87015c57f0fe6e360d1b9d.jpg"
-				resName="Barn"
-				ratings={4.6}
-				tags= {["hello", "hi"]}
-			/>
-    	</>
+		<Routes>
+			<Route path="/" element={<Landing />} />
+		</Routes>
+
   	)
 }
 
