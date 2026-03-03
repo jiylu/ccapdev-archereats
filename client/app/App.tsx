2147499bea303946367854b7e84a47762aa31662
Gaibril <gaibrilgregorio@gmail.com>
2026-03-03 14:44:43 +0800

    fixed file names

--- client/app/App.tsx
+++ client/app/App.tsx
@@ -1,9 +1,9 @@
 //import { useState } from 'react'
 
 import { Route, Routes } from "react-router-dom"
-import Profile from "./pages/Profile"
-import AddFood from "./pages/Add-Food"
-import Favorites from "./pages/Favorites"
+import Profile from "./pages/profile/profile"
+import AddFood from "./pages/add-food"
+import Favorites from "./pages/favorites"
 import Landing from "./pages/landing"
 import Directory from "./pages/directory/directory"
 
