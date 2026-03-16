2f611fe54ca87fec6dddd74174950cd6ea501f22
LaSalsaEsDeliciosa <lovalentine14@gmail.com>
2026-03-17 00:27:09 +0800

    Skeleton Done, Tailwind later

--- client/app/pages/landing.tsx
+++ client/app/pages/landing.tsx
@@ -1,7 +1,22 @@
 import Navbar from "../components/layout/navbar";
+import Footer from "../components/layout/footer";
+import { useEffect } from "react";
 
 export default function Landing () {
+    useEffect(() => {
+        document.title="Home | ArcherEats";
+    }, [])
+
     return (
-        <Navbar />
+        <div className="landing-page">
+            <Navbar />
+            <div className="main-section">
+                
+            </div>
+            <div className="feature-section">
+
+            </div>
+            <Footer />
+        </div>
     )
 }
