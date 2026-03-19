cd4ec732cfea34cb5517b289e56804ec4544efa2
jilyu <jeremy_leano@dlsu.edu.ph>
2026-03-19 16:39:59 +0800

    added sorting feature

--- client/app/pages/directory/directory.tsx
+++ client/app/pages/directory/directory.tsx
@@ -13,7 +13,7 @@ export default function Directory () {
     const [restaurants, setRestaurants] = useState<Restaurant[]>([])
     const [currentPage, setCurrentPage] = useState(1);
     const [loading, setLoading] = useState(true);
-
+    const [sortOption, setSortOption] = useState<string>("");
 
     useEffect(() => {
         const fetchRestaurants = async () => {
@@ -39,12 +39,29 @@ export default function Directory () {
     
     if (loading) return <PageLoader />;
 
+
+    const sortedRestaurants = [...restaurants].sort((a,b) => {
+        if (sortOption === "highestRating") {
+            return (b.avgRating ?? 0) - (a.avgRating ?? 0);
+        }
+
+        if (sortOption === "mostPopular") {
+            return (b.amtRatings ?? 0) - (a.amtRatings ?? 0);
+        }
+
+        if (sortOption === "none") {
+            return 0;
+        }
+
+        return 0;
+    })
+
     const itemsPerPage = 6;
     const pageAmt = Math.ceil(restaurants.length / itemsPerPage);
     
     const start = (currentPage - 1) * itemsPerPage;
     const end = start + itemsPerPage;
-    const currentRestaurants = restaurants.slice(start, end);
+    const currentRestaurants = sortedRestaurants.slice(start, end);
 
     // max sa pagination
     const maxVisible = 5;
@@ -72,7 +89,6 @@ export default function Directory () {
     }
 
 
-
     return (
         <div className="flex flex-col min-h-screen">
             <Navbar />
@@ -86,15 +102,16 @@ export default function Directory () {
                             <span className="font-semibold">Showing {currentPage} of {pageAmt} page(s)</span>
                             <div className="flex items-center">
                                 <span className="mr-2.5 whitespace-nowrap font-semibold">Sort By:</span>
-                                <Select>
+                                <Select onValueChange={(value) => setSortOption(value)}>
                                     <SelectTrigger className="w-full max-w-48">
-                                        <SelectValue placeholder="" />
+                                        <SelectValue placeholder="None" />
                                     </SelectTrigger>
                                     <SelectContent>
                                         <SelectGroup>
                                             <SelectLabel>Filters</SelectLabel>
                                             <SelectItem value="highestRating">Sort by Highest Rating</SelectItem>
                                             <SelectItem value="mostPopular">Sort by Most Popular</SelectItem>
+                                            <SelectItem value="none">None</SelectItem>
                                         </SelectGroup>
                                     </SelectContent>
                                 </Select>
