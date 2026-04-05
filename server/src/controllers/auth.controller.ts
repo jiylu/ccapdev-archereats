ce8ce961468ffe2d51c0c0cc8ab6bfcec28b81df
jilyu <jeremy_leano@dlsu.edu.ph>
2026-04-05 22:12:12 +0800

    Update auth.controller.ts

--- server/src/controllers/auth.controller.ts
+++ server/src/controllers/auth.controller.ts
@@ -36,7 +36,7 @@ export const loginUser = async (req: Request<object, object, AuthData>, res: Res
         res.cookie('token', token, {
             httpOnly: true,
             secure: process.env.NODE_ENV === 'production',
-            sameSite: 'strict',
+            sameSite: 'none',
             maxAge: 7 * 24 * 60 * 60 * 1000
         })
 
