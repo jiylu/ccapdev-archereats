1166e3a64b7f70dd94ee92b934b987c8580e3419
jilyu <jeremy_leano@dlsu.edu.ph>
2026-04-08 22:10:32 +0800

    Update about-us.tsx

--- client/app/pages/about-us.tsx
+++ client/app/pages/about-us.tsx
@@ -1,6 +1,6 @@
 import Navbar from "../components/layout/navbar";
 import Footer from "../components/layout/footer";
-import { Github, Instagram, Facebook } from "lucide-react";
+import { Github } from "lucide-react";
 import { useEffect } from "react";
 
 const teamMembers = [
